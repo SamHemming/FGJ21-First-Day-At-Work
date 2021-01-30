8dc94c4a0b6fdc4b10f69a839d71a22e3d1d3b44
SamHemming <32447304+SamHemming@users.noreply.github.com>
2021-01-30 03:16:38 +0200

    FOV nad map
    Added FOV effect
    Map made from paliks
    Player movement fixed to use collisions
    Many prefabs and magic :)

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-	[SerializeField, Range(0.01f, 1f)] private float movementSpeed = 1f;
+	[SerializeField, Range(0.01f, 2f)] private float movementSpeed = 1f;
 	[SerializeField, Range(0, 360)] private int angleOffset = 0;
+
+	private Rigidbody2D rb;
+
+	private void Awake()
+	{
+		rb = this.GetComponent<Rigidbody2D>();
+	}
 
 	private void FixedUpdate()
 	{
-		this.transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
+		//this.transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
 		
+		rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
+
 		//Not mine, copy-paste ain't no sin :DD
 		var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(this.transform.position);
 		var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + angleOffset;
