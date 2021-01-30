545cb832ce8b0313c56d8403c1c8348b230e6ede
Mjarsk <59711150+Mjarsk@users.noreply.github.com>
2021-01-30 03:16:26 +0200

    Itemstuffs
    kiiti sam hyvät itemit ja systeemit 5/5
    
    nyt voi pelaaja pidellä itemiä stringeissään
    
    Co-Authored-By: SamHemming <32447304+SamHemming@users.noreply.github.com>

---
+++ Assets/Scripts/DialogueTriggers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggers : MonoBehaviour
+{
+    public bool ballCarry;
+    public GameObject ballText;
+    void Start()
+    {
+        ballText.SetActive(false);
+        ballCarry = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        print("lol");
+        if (player.CompareTag("Player") && (!ballCarry))
+        {
+            ballText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D player)
+    {
+        if (player.CompareTag("Player") && (Input.GetKey(KeyCode.Space)))
+        {
+            ballText.SetActive(false);
+            ballCarry = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            ballText.SetActive(false);
+        }
+    }
+}
